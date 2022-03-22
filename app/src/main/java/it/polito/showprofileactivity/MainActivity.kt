566a1acbbeea8d1008f1e0548ea687ea5092727c
Pietro Borgaro <pietro.borgaro@gmail.com>
2022-03-22 16:13:16 +0100

    profileActivity icons changed

--- app/src/main/java/it/polito/showprofileactivity/MainActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/MainActivity.kt
@@ -1,16 +1,38 @@
 package it.polito.showprofileactivity
 
+import android.content.Intent
 import android.graphics.drawable.Icon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.edit_button, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.edit_button -> {
+                //call edit activity
+                val intent = Intent(this, EditProfileActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }

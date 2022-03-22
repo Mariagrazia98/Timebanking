d57d911c8ff7a5746d32f9566ddac004a16e470e
Mariagrazia <mariagrazia.paladino1@gmail.com>
2022-03-22 10:48:20 +0100

    Fixed portrait layout

--- app/src/main/java/it/polito/showprofileactivity/MainActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/MainActivity.kt
@@ -1,5 +1,6 @@
 package it.polito.showprofileactivity
 
+import android.graphics.drawable.Icon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,11 +11,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.game_menu, menu)
-        return true
-    }*/
 
 }

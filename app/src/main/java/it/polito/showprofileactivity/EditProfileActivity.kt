3f2c74ac5b8fd5e803bf26f3e650100a9cdbd03c
ANTONIO VESPA s284101 <44071720+mrwasp98@users.noreply.github.com>
2022-03-27 15:15:57 +0200

    Revert "update"
    This reverts commit 12a404884ef045973a35bf27865ee2b6cad64652.

--- app/src/main/java/it/polito/showprofileactivity/EditProfileActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/EditProfileActivity.kt
@@ -6,11 +6,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.view.ContextMenu
-import android.view.MenuInflater
-import android.view.View
 import android.widget.EditText
-import android.widget.ImageView
 import androidx.core.widget.doOnTextChanged
 
 class EditProfileActivity : AppCompatActivity() {
@@ -39,10 +35,6 @@ class EditProfileActivity : AppCompatActivity() {
         email_view.setText(email)
         var location_view = findViewById<EditText>(R.id.Edit_Location)
         location_view.setText(location)
-
-        //floating context menù
-        var image_view = findViewById<ImageView>(R.id.imageView_Edit)
-        registerForContextMenu(image_view)
 
         ///aggiungo listener
         fullname_view.addTextChangedListener(object : TextWatcher {
@@ -90,13 +82,4 @@ class EditProfileActivity : AppCompatActivity() {
         sendBroadcast(intent)
         //Log.i(info,data)
     }
-
-    override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenu.ContextMenuInfo){
-        super.onCreateContextMenu(menu, v, menuInfo)
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.context_menu, menu)
-    }
 }
-
-
-

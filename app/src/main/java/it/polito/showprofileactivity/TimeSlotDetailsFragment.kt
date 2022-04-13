1ba97cdbf265fcd9799818795961fe9d25a04882
Mariagrazia <mariagrazia.paladino1@gmail.com>
2022-04-13 10:28:18 +0200

    Created options menu

--- app/src/main/java/it/polito/showprofileactivity/TimeSlotDetailsFragment.kt
+++ app/src/main/java/it/polito/showprofileactivity/TimeSlotDetailsFragment.kt
@@ -1,10 +1,10 @@
 package it.polito.showprofileactivity
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +29,24 @@ class TimeSlotDetailsFragment : Fragment() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_time_slot_details, container, false)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
     companion object {

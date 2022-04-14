ffdf8c004d6d250db0d13a6b0f9516f25898b5d5
Pietro Borgaro <pietro.borgaro@gmail.com>
2022-04-14 10:07:23 +0200

    advertisement edit mode

--- app/src/main/java/it/polito/timebanking/TimeSlotEditFragment.kt
+++ app/src/main/java/it/polito/timebanking/TimeSlotEditFragment.kt
@@ -1,10 +1,15 @@
 package it.polito.timebanking
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import java.text.SimpleDateFormat
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +34,60 @@ class TimeSlotEditFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_time_slot_edit, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val cal = Calendar.getInstance()
+
+        //DATE
+        val dateButton: Button  = view.findViewById(R.id.edit_dateAdvertisement)
+        dateButton.text = SimpleDateFormat("dd/MM/yyyy", Locale.ITALY).format(System.currentTimeMillis())
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd/MM/yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            dateButton.text = sdf.format(cal.time)
+        }
+
+        dateButton.setOnClickListener {
+            context?.let { it1 ->
+                DatePickerDialog(
+                    it1, dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+
+        //TIME
+        val timeButton: Button = view.findViewById(R.id.edit_timeAdvertisement)
+
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+
+            val sdf = SimpleDateFormat("HH:mm", Locale.ITALY)
+            timeButton.text = sdf.format(cal.time)
+        }
+
+        timeButton.setOnClickListener {
+            context?.let { it1 ->
+                TimePickerDialog(
+                    it1, timeSetListener,
+                    cal.get(Calendar.HOUR_OF_DAY),
+                    cal.get(Calendar.MINUTE), true).show()
+            }
+        }
+
     }
 
     companion object {

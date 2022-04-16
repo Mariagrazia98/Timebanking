0aeada00de97474de508a2b30abda41797b54ecd
Antonio Vespa <44071720+mrwasp98@users.noreply.github.com>
2022-04-16 10:16:37 +0200

    update vm

--- app/src/main/java/it/polito/timebanking/repository/MyViewModel.kt
+++ app/src/main/java/it/polito/timebanking/repository/MyViewModel.kt
@@ -1,6 +1,54 @@
 package it.polito.timebanking.repository
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import kotlin.concurrent.thread
 
-class MyViewModel: ViewModel() {
+class MyViewModel(application: Application): AndroidViewModel(application) {
+    val repo = Repository(application)
+    val users: LiveData<List<User>>? = repo.getAllUsers()
+    val slots: LiveData<List<Slot>>? = repo.getAllSlots()
+
+    //users
+    fun addUser(fullname:String,nickname:String,email:String,location:String){
+        thread {
+            repo.addUser(fullname,nickname,email,location)
+        }
+    }
+
+    fun getUserById(id:String) = repo.getUserById(id)
+
+    fun removeUser(id:String){
+        thread {
+            repo.removeUserById(id)
+        }
+    }
+
+    fun clearUsers(){
+        thread {
+            repo.clearAllUSers()
+        }
+    }
+    //slots
+    fun addSlot(title:String,description:String,date:String,time:String,duration: Int, location: String){
+        thread {
+            repo.addSlot(title,description,date,time,duration,location)
+        }
+    }
+
+    fun getSlotById(id:String) = repo.getSlotById(id)
+
+    fun removeSlot(id:String){
+        thread {
+            repo.removeSlotById(id)
+        }
+    }
+
+    fun clearSlots(){
+        thread {
+            repo.clearAllSlots()
+        }
+    }
 }
--- app/src/main/java/it/polito/timebanking/repository/Repository.kt
+++ app/src/main/java/it/polito/timebanking/repository/Repository.kt
@@ -1,13 +1,47 @@
 package it.polito.timebanking.repository
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import java.time.Duration
 
 class Repository(application: Application) {
     private val userDao = UserDatabase.getDatabase(application)?.userDao()
     private val slotDao = SlotDatabase.getDatabase(application)?.slotDao()
 
+    //users
     fun addUser(fullname:String,nickname:String,email:String,location:String){
         val u = User().also{ it.fullname=fullname; it.email=email; it.location=location; it.nickname=nickname}
         userDao?.addUser(u)
     }
+
+    fun clearAllUSers(){
+        userDao?.removeAll()
+    }
+
+    fun removeUserById(id: String){
+        userDao?.removeUserById(id)
+    }
+
+    fun getAllUsers(): LiveData<List<User>>? = userDao?.findAll()
+
+    fun getUserById(id:String): LiveData<User>? = userDao?.searchUserByID(id)
+
+    //slots
+    fun addSlot(title:String,description:String,date:String,time:String,duration: Int, location: String){
+        val s = Slot().also{ it.title=title; it.description=description; it.date=date; it.time=time; it.duration=duration; it.location=location}
+        slotDao?.addSlot(s)
+    }
+
+    fun clearAllSlots(){
+        slotDao?.removeAll()
+    }
+
+    fun removeSlotById(id: String){
+        slotDao?.removeSlotById(id)
+    }
+
+    fun getAllSlots(): LiveData<List<Slot>>? = slotDao?.findAll()
+
+    fun getSlotById(id:String): LiveData<Slot>? = slotDao?.searchSlotByID(id)
+
 }

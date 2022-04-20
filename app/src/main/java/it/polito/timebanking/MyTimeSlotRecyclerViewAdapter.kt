399552ac5a4373d2f74aafc7152b709d51215c06
Pietro Borgaro <pietro.borgaro@gmail.com>
2022-04-20 16:05:49 +0200

    code cleaning

--- app/src/main/java/it/polito/timebanking/MyTimeSlotRecyclerViewAdapter.kt
+++ app/src/main/java/it/polito/timebanking/MyTimeSlotRecyclerViewAdapter.kt
@@ -4,15 +4,11 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.FragmentManager
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import it.polito.timebanking.placeholder.PlaceholderContent.PlaceholderItem
 
 /**

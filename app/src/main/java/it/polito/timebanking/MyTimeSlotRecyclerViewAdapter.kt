640ef1dae152ae10b7f2de473d2e5c78d671de74
Mariagrazia <mariagrazia.paladino1@gmail.com>
2022-04-27 17:28:53 +0200

    Updated onBindViewHolder method

--- app/src/main/java/it/polito/timebanking/MyTimeSlotRecyclerViewAdapter.kt
+++ app/src/main/java/it/polito/timebanking/MyTimeSlotRecyclerViewAdapter.kt
@@ -12,10 +12,11 @@ import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import it.polito.timebanking.repository.Slot
 
-class MyTimeSlotRecyclerViewAdapter(val data: List<Slot>): RecyclerView.Adapter<MyTimeSlotRecyclerViewAdapter.ItemSlotViewHolder>() {
+class MyTimeSlotRecyclerViewAdapter(val data: List<Slot>) :
+    RecyclerView.Adapter<MyTimeSlotRecyclerViewAdapter.ItemSlotViewHolder>() {
     var list = data.toMutableList()
 
-    class ItemSlotViewHolder(v: View): RecyclerView.ViewHolder(v){
+    class ItemSlotViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         private val title: TextView = v.findViewById(R.id.slot_title)
         private val date: TextView = v.findViewById(R.id.slot_date)
         private val time: TextView = v.findViewById(R.id.slot_time)
@@ -23,47 +24,45 @@ class MyTimeSlotRecyclerViewAdapter(val data: List<Slot>): RecyclerView.Adapter<
         val cv: CardView = v.findViewById(R.id.cv)
         val button: ImageButton = v.findViewById(R.id.button)
 
-        fun bind(item: Slot, action: (v: View)->Unit){
+        fun bind(item: Slot) {
             title.text = item.title
             date.text = item.date
             time.text = item.time
             duration.text = item.duration.toString()
 
-            var bundle = bundleOf("id" to item.id)
-
-            cv.setOnClickListener{
-                findNavController(FragmentManager.findFragment(it)).navigate(R.id.action_timeSlotListFragment_to_timeSlotDetailsFragment, bundle)
-            }
-
-            button.setOnClickListener{
-                findNavController(FragmentManager.findFragment(it)).navigate(R.id.action_timeSlotListFragment_to_timeSlotEditFragment2, bundle)
-            }
 
         }
 
-        fun unbind(){}
+        fun unbind() {}
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemSlotViewHolder {
-        val vg = LayoutInflater.from(parent.context).inflate(R.layout.fragment_time_slot, parent, false)
+        val vg =
+            LayoutInflater.from(parent.context).inflate(R.layout.fragment_time_slot, parent, false)
         return ItemSlotViewHolder(vg)
     }
 
     override fun onBindViewHolder(holder: ItemSlotViewHolder, position: Int) {
-        val item = list[position]
 
-        holder.bind(item) {
-            val pos = list.indexOf(item)
-            if (pos!=-1) {
-                list.removeAt(pos)
-                val pos1 = list.indexOf(item)
-                if (pos1!= -1) {
-                    list.removeAt(pos1)
-                    notifyItemRemoved(pos1)
-                }
-            }
+        val item = list[position]
+        item.let { holder.bind(it) }
+        var bundle = bundleOf("id" to item.id)
+
+        holder.cv.setOnClickListener {
+            findNavController(FragmentManager.findFragment(it)).navigate(
+                R.id.action_timeSlotListFragment_to_timeSlotDetailsFragment,
+                bundle
+            )
+        }
+
+        holder.button.setOnClickListener {
+            findNavController(FragmentManager.findFragment(it)).navigate(
+                R.id.action_timeSlotListFragment_to_timeSlotEditFragment2,
+                bundle
+            )
         }
     }
+
 
     override fun getItemCount(): Int = list.size
 

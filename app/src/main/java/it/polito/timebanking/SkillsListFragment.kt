a37aa16167aeb4f6768284188a1eff191d725b8a
Antonio Vespa <44071720+mrwasp98@users.noreply.github.com>
2022-05-08 22:19:55 +0200

    added search button

--- app/src/main/java/it/polito/timebanking/SkillsListFragment.kt
+++ app/src/main/java/it/polito/timebanking/SkillsListFragment.kt
@@ -1,29 +1,24 @@
 package it.polito.timebanking
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import it.polito.timebanking.viewmodel.TimeSlotViewModel
+
 
 class SkillsListFragment : Fragment() {
     lateinit var timeSlotVM: TimeSlotViewModel
 
     val skillList = listOf("Android developer", "Electrician", "Gardening", "Bicycle repairer", "Babysitter")
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_skills_list, container, false)
         timeSlotVM = ViewModelProvider(requireActivity()).get(TimeSlotViewModel::class.java)
-
+        setHasOptionsMenu(true)
         //timeSlotVM.clearSlots() //to clear the repo uncomment this and run the app
 
         val ev: TextView = view.findViewById(R.id.empty_view)
@@ -42,5 +37,16 @@ class SkillsListFragment : Fragment() {
         }
 
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_button, menu);
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        //delete edit_button
+        menu.findItem(R.id.edit_button).isVisible = false
+        super.onPrepareOptionsMenu(menu)
     }
 }

858e85a9877260393067b7b20626813afd8c2c79
Antonio Vespa <44071720+mrwasp98@users.noreply.github.com>
2022-05-23 23:20:55 +0200

    splash screen

---
+++ app/src/main/java/it/polito/timebanking/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+
+
+package it.polito.timebanking
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import it.polito.timebanking.MainActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler().postDelayed(Runnable { //This method will be executed once the timer is over
+            // Start your app main activity
+            val i = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(i)
+            // close this activity
+            finish()
+        }, 3000)
+    }
+}

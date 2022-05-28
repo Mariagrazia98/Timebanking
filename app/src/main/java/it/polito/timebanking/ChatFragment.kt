94041c6b36cc4bce4e3bef384a42448de18a80f5
Pietro Borgaro <pietro.borgaro@gmail.com>
2022-05-28 11:47:59 +0200

    assign question removed when not needed

--- app/src/main/java/it/polito/timebanking/ChatFragment.kt
+++ app/src/main/java/it/polito/timebanking/ChatFragment.kt
@@ -86,6 +86,7 @@ class ChatFragment : Fragment() {
                     if((userId!=userOfferer.uid) || (chat!=null && chat!!.chatStatus==1)){
                         assignButton.visibility=View.GONE
                         rejectButton.visibility=View.GONE
+                        titleChat.visibility=View.GONE
                     }
                     if(chat!=null && chat!!.chatStatus==1){
                         titleChat.setText("This timeslot request was rejected!")

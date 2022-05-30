8df567daf6066fd0d4089b0c2d93cc6d259fee64
Antonio Vespa <44071720+mrwasp98@users.noreply.github.com>
2022-05-30 10:04:38 +0200

    fix skill list

--- app/src/main/java/it/polito/timebanking/ChatFragment.kt
+++ app/src/main/java/it/polito/timebanking/ChatFragment.kt
@@ -1,8 +1,12 @@
 package it.polito.timebanking
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.*
 import android.widget.*
+import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -158,6 +162,20 @@ class ChatFragment : Fragment() {
             }
         }
 
+        requireActivity()
+            .onBackPressedDispatcher
+            .addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    (activity as MainActivity).lastSkill = ""
+                    // if you want onBackPressed() to be called as normal afterwards
+                    if (isEnabled) {
+                        isEnabled = false
+                        requireActivity().onBackPressed()
+                    }
+                }
+            }
+            )
+
         return view
     }
 
@@ -245,4 +263,6 @@ class ChatFragment : Fragment() {
         )
         snackbar.show()
     }
+
+
 }
